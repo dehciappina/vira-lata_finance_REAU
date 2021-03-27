35e29fbf889feff71e25bbfa779654730d484d07
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-03-27 02:24:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const header = document.querySelector('header')
 
 window.onscroll = function() {
-    if(document.documentElement.scrollTop > (window.innerHeight /2)) {
+    if(document.documentElement.scrollTop > (window.innerHeight /3)) {
         header.style.transform = 'translateY(0)';
     } else {
         header.style.transform = 'translateY(-100%)';
     }
 }
-
-
-
-
-ScrollReveal().reveal('.scrollrv', {
-    distance: '30px',
-    duration: 1400,
-    easing: 'ease',
-})
